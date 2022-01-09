d4755bb0378ff1a2f9b4e544def1c8dc6a2fc74b
Edward Soetiono <edward_soetiono@estl.sg>
2022-01-09 18:18:09 +0800

    LSP

--- lua/configs/lsp/init.lua
+++ lua/configs/lsp/init.lua
@@ -1,37 +1,3 @@
 require("configs.lsp.lsp-installer")
+require("configs.lsp.handlers").setup()
 require("configs.lsp.null-ls")
-
-local nvim_lsp = require("lspconfig")
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-local on_attach = function(client, bufnr)
-	if client.name == "tsserver" then
-		-- disable tsserver formatting in favor of null-ls
-		client.resolved_capabilities.document_formatting = false
-	end
-
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-
-	local opts = { noremap = true, silent = true }
-
-	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-end
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
-
-local servers = { "tsserver", "sumneko_lua", "html" }
-for _, lsp in ipairs(servers) do
-	nvim_lsp[lsp].setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-	})
-end
--- lua/configs/lsp/lsp-installer.lua
+++ lua/configs/lsp/lsp-installer.lua
@@ -1,7 +1,10 @@
 local lsp_installer = require("nvim-lsp-installer")
 
 lsp_installer.on_server_ready(function(server)
-	local opts = {}
+	local opts = {
+		on_attach = require("configs.lsp.handlers").on_attach,
+		capabilities = require("configs.lsp.handlers").capabilities,
+	}
 
 	if server.name == "sumneko_lua" then
 		local sumneko_opts = require("configs.lsp.settings.sumneko_lua")
