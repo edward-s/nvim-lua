c6ca3b6908cdadb7159d3fe983d7702852bf53d5
Edward Soetiono <edward_soetiono@estl.sg>
2022-03-23 15:51:49 +0800

    fixed code folding

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,6 @@
-vim.g.mapleader = " "
-
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
+vim.g.mapleader = " "
 
 -- Split navigations
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -10,27 +9,33 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize windows
-keymap("n", "<A-j>", ":resize -2<CR>", opts)
-keymap("n", "<A-k>", ":resize +2<CR>", opts)
-keymap("n", "<A-h>", ":vertical resize -2<CR>", opts)
-keymap("n", "<A-l>", ":vertical resize +2<CR>", opts)
+keymap("n", "<A-j>", ":resize -2<cr>", opts)
+keymap("n", "<A-k>", ":resize +2<cr>", opts)
+keymap("n", "<A-h>", ":vertical resize -2<cr>", opts)
+keymap("n", "<A-l>", ":vertical resize +2<cr>", opts)
 
 -- Indentation
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
+-- Code folding
+keymap("n", "zc", "zc<cmd>IndentBlanklineRefresh<cr>", opts)
+keymap("n", "za", "za<cmd>IndentBlanklineRefresh<cr>", opts)
+keymap("n", "zr", "zr<cmd>IndentBlanklineRefresh<cr>", opts)
+keymap("n", "zR", "zR<cmd>IndentBlanklineRefresh<cr>", opts)
+
 -- Fugitive
-keymap("n", "<leader>df", ":diffget //2<CR>", opts)
-keymap("n", "<leader>dj", ":diffget //3<CR>", opts)
+keymap("n", "<leader>df", ":diffget //2<cr>", opts)
+keymap("n", "<leader>dj", ":diffget //3<cr>", opts)
 
 -- Nvim tree
-keymap("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", opts)
+keymap("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", opts)
 
 -- Telescope
 keymap(
 	"n",
 	"<C-p>",
-	"<cmd>lua require('telescope.builtin').git_files(require('telescope.themes').get_dropdown{ previewer = false })<CR>",
+	"<cmd>lua require('telescope.builtin').git_files(require('telescope.themes').get_dropdown{ previewer = false })<cr>",
 	opts
 )
 
