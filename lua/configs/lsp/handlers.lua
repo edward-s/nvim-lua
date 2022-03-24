afab4a2d59b1733c8b172ca41b4e79ded7dde3ae
Edward Soetiono <edward_soetiono@estl.sg>
2022-03-24 21:04:28 +0800

    fixed document highlighting

--- lua/configs/lsp/handlers.lua
+++ lua/configs/lsp/handlers.lua
@@ -42,22 +42,6 @@ M.setup = function()
 	})
 end
 
-local function lsp_highlight_document(client)
-	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
-		vim.api.nvim_exec(
-			[[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-			false
-		)
-	end
-end
-
 M.on_attach = function(client)
 	if client.name == "tsserver" then
 		client.resolved_capabilities.document_formatting = false
@@ -69,8 +53,6 @@ M.on_attach = function(client)
 		})
 		ts_utils.setup_client(client)
 	end
-
-	lsp_highlight_document(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
