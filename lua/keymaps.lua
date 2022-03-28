2e46d9512dba0e00288ca07501b7f37870b335b7
Edward Soetiono <edward_soetiono@estl.sg>
2022-03-28 11:28:03 +0800

    fixed lightspeed macro

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -44,3 +44,11 @@ keymap(
 -- Bufferline
 keymap("n", "<Tab>", "<cmd>bnext<cr>", opts)
 keymap("n", "<S-Tab>", "<cmd>bprevious<cr>", opts)
+
+-- Lightspeed
+vim.cmd([[
+  nmap <expr> f reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_f" : "f"
+  nmap <expr> F reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_F" : "F"
+  nmap <expr> t reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_t" : "t"
+  nmap <expr> T reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_T" : "T"
+]])
