68764544c8be603b51f3619d71652742cca75a14
Edward Soetiono <sedward@live.com.sg>
2022-04-05 21:00:39 +0800

    removed ultest

--- lua/configs/test.lua
+++ lua/configs/test.lua
@@ -1,10 +1,6 @@
 local g = vim.g
 
 g["test#strategy"] = "neovim"
-g["test#neovim#term_position"] = "bel 15"
+g["test#neovim#term_position"] = "belowright 15"
 g["test#preserve_screen"] = 1
 g["ultest_use_pty"] = 1
-g["ultest_summary_height"] = 10
-g["ultest_output_on_run"] = 0
-g["ultest_output_on_line"] = 0
-g["ultest_summary_open"] = "botright split | resize 10"
