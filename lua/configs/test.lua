725857dffe21f7df12ff6aa43e9253b916a1ea09
Edward <edward_soetiono@estl.sg>
2022-05-28 19:38:18 +0800

    fixed test summary

--- lua/configs/test.lua
+++ lua/configs/test.lua
@@ -4,5 +4,4 @@ g["ultest_max_threads"] = 8
 g["ultest_use_pty"] = 1
 g["ultest_output_on_run"] = 0
 g["ultest_output_on_line"] = 0
-g["ultest_summary_height"] = 10
-g["ultest_summary_open"] = "botright split"
+g["ultest_summary_open"] = "botright split | resize 10"
