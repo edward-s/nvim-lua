66b72a1b618f4003268fcb447e085ff085040b00
Edward <edward_soetiono@estl.sg>
2022-07-19 21:43:41 +0800

    diffview fixes

--- lua/configs/diffview.lua
+++ lua/configs/diffview.lua
@@ -1,12 +1,11 @@
 local lib = require("diffview.lib")
-local diffview = require("diffview")
 
 local M = {}
 
 M.toggle = function()
   local view = lib.get_current_view()
   if view == nil then
-    diffview.file_history();
+    vim.cmd(':DiffviewFileHistory %');
     return
   end
 
