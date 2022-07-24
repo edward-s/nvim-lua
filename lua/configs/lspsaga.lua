df10e67c606ca7db7782af4807595ea3ea662428
Edward <edward_soetiono@estl.sg>
2022-07-24 14:20:20 +0800

    added neoscroll

--- lua/configs/lspsaga.lua
+++ lua/configs/lspsaga.lua
@@ -2,9 +2,4 @@ local saga = require 'lspsaga'
 
 saga.init_lsp_saga({
   border_style = "rounded",
-  code_action_keys = {
-    quit = "<Esc>",
-  },
-  rename_action_quit = "<Esc>",
-  saga_winblend = 0
 })
