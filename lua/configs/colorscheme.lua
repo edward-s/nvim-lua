9200d3bd7cdc6a44bc00b5dba69f201d47fc3dc9
Edward <edward_soetiono@estl.sg>
2022-08-03 00:48:16 +0800

    override gruvbox material highlights

--- lua/configs/colorscheme.lua
+++ lua/configs/colorscheme.lua
@@ -1,1 +1,17 @@
-vim.cmd([[ colorscheme gruvbox-material ]])
+vim.cmd([[ 
+
+function! s:gruvbox_material_custom() abort
+  highlight! link DiagnosticWarn None
+  highlight! link DiagnosticError None
+  highlight! link DiagnosticInfo None
+  highlight! link DiagnosticHint None
+endfunction
+
+augroup GruvboxMaterialCustom
+  autocmd!
+  autocmd ColorScheme gruvbox-material call s:gruvbox_material_custom()
+augroup END
+
+colorscheme gruvbox-material
+
+]])
