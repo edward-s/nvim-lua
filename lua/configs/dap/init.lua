4a8ee8f8981dc4e738911452bd23b57781b593a1
Edward <edward_soetiono@estl.sg>
2022-09-24 20:58:37 +0800

    dap minor fix

--- lua/configs/dap/init.lua
+++ lua/configs/dap/init.lua
@@ -1,3 +1,5 @@
+local dap = require("dap")
+
 local dap_breakpoint = {
 	error = {
 		text = "🟥",
@@ -24,6 +26,7 @@ vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
 vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
 
 require("nvim-dap-virtual-text").setup({
+	all_frames = true,
 	commented = true,
 })
 require("dapui").setup()
@@ -33,7 +36,6 @@ require("dap-vscode-js").setup({
 	debugger_path = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/js-debug-adapter",
 	adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
 })
-local dap = require("dap")
 
 for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
 	dap.configurations[language] = {
