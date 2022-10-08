614e0ef2d6f27f89cc13f5a7d63935634d29f350
Edward <edward_soetiono@estl.sg>
2022-10-08 23:55:48 +0800

    removed gruvbox material hacks colors

--- lua/configs/colorscheme.lua
+++ lua/configs/colorscheme.lua
@@ -1,17 +1,4 @@
 vim.cmd([[ 
-
-function! s:gruvbox_material_custom() abort
-  highlight! link DiagnosticWarn None
-  highlight! link DiagnosticError None
-  highlight! link DiagnosticInfo None
-  highlight! link DiagnosticHint None
-endfunction
-
-augroup GruvboxMaterialCustom
-  autocmd!
-  autocmd ColorScheme gruvbox-material call s:gruvbox_material_custom()
-augroup END
-
 let g:gruvbox_material_visual = 'green background'
 
 colorscheme gruvbox-material
