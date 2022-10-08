5fe5939e7675693cc9db0e5f4bdaa55b6282518a
Edward <edward_soetiono@estl.sg>
2022-10-08 19:34:52 +0800

    fixed telescope project file search

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -53,10 +53,11 @@ end)
 local M = {}
 
 M.project_files = function()
-	local opts = {}
-	local ok = pcall(require("telescope.builtin").git_files, opts)
-	if not ok then
-		require("telescope.builtin").find_files(opts)
+	local in_git_repo = vim.fn.systemlist("git rev-parse --is-inside-work-tree")[1] == "true"
+	if in_git_repo then
+		require("telescope.builtin").git_files()
+	else
+		require("telescope.builtin").find_files()
 	end
 end
 
