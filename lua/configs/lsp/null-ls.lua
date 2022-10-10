dff407fdde8a2adbff4818506efd211ead1dc007
Edward <edward_soetiono@estl.sg>
2022-10-10 09:21:09 +0800

    added gofmt

--- lua/configs/lsp/null-ls.lua
+++ lua/configs/lsp/null-ls.lua
@@ -4,20 +4,21 @@ local lsputils = require("configs.lsp.utils")
 local null_ls = require("null-ls")
 
 function M.setup()
-  local formatting = null_ls.builtins.formatting
-  local codeActions = null_ls.builtins.code_actions
-  local diagnostics = null_ls.builtins.diagnostics
-  local sources = {
-    formatting.prettierd,
-    formatting.stylua,
-    codeActions.eslint_d,
-    diagnostics.eslint_d,
-  }
-  null_ls.setup({
-    sources = sources,
-    on_attach = lsputils.lsp_attach,
-    capabilities = lsputils.get_capabilities(),
-  })
+	local formatting = null_ls.builtins.formatting
+	local codeActions = null_ls.builtins.code_actions
+	local diagnostics = null_ls.builtins.diagnostics
+	local sources = {
+		formatting.prettierd,
+		formatting.stylua,
+		formatting.gofmt,
+		codeActions.eslint_d,
+		diagnostics.eslint_d,
+	}
+	null_ls.setup({
+		sources = sources,
+		on_attach = lsputils.lsp_attach,
+		capabilities = lsputils.get_capabilities(),
+	})
 end
 
 return M
