356251c6f2cb6da35a70be2a7c208736090e063e
Edward <edward_soetiono@estl.sg>
2022-10-12 09:42:38 +0800

    fixed colorscheme

--- lua/configs/colorscheme.lua
+++ lua/configs/colorscheme.lua
@@ -1,16 +1,17 @@
 vim.cmd([[ 
-    highlight! link DiagnosticWarn None
-    highlight! link DiagnosticError None
-    highlight! link DiagnosticInfo None
-    highlight! link DiagnosticHint None
-  endfunction
+function! s:gruvbox_material_custom() abort
+  highlight! link DiagnosticWarn None
+  highlight! link DiagnosticError None
+  highlight! link DiagnosticInfo None
+  highlight! link DiagnosticHint None
+endfunction
 
-  augroup GruvboxMaterialCustom
-    autocmd!
-    autocmd ColorScheme gruvbox-material call s:gruvbox_material_custom()
-  augroup END
+augroup GruvboxMaterialCustom
+  autocmd!
+  autocmd ColorScheme gruvbox-material call s:gruvbox_material_custom()
+augroup END
 
-  let g:gruvbox_material_visual = 'green background'
+let g:gruvbox_material_visual = 'green background'
 
-  colorscheme gruvbox-material
+colorscheme gruvbox-material
 ]])
