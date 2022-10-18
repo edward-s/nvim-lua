1948a8f87de4185711026987ff9710251f24997e
Edward <edward_soetiono@estl.sg>
2022-10-18 18:56:27 +0800

    cleaned up dap adapters

--- lua/configs/lsp/settings/sumneko_lua.lua
+++ lua/configs/lsp/settings/sumneko_lua.lua
@@ -7,7 +7,7 @@ function M.setup()
 		settings = {
 			Lua = {
 				diagnostics = {
-					globals = { "vim", "use", "unpack", "require" },
+					globals = { "vim", "use", "unpack" },
 				},
 				workspace = {
 					library = {
