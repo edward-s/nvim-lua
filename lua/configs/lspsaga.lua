6512e443d2da4973dcf6c2c6d0f1233d75412fe2
Edward <edward_soetiono@estl.sg>
2023-01-14 20:23:58 +0800

    new lspsaga setuo

--- lua/configs/lspsaga.lua
+++ lua/configs/lspsaga.lua
@@ -1,11 +1,7 @@
 local lspsaga = require("lspsaga")
 
 lspsaga.setup({
-	border_style = "rounded",
-	finder_action_keys = {
-		open = "<Enter>",
-	},
-	show_outline = {
-		jump_key = "<Enter>",
+	ui = {
+		border = "rounded",
 	},
 })
