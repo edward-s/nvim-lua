b7f4e38657d269edfde0fc8d38836741976ffb56
Edward <edward_soetiono@estl.sg>
2023-02-24 07:40:43 +0800

    fixed minor warning

--- lua/configs/lsp/settings/tsserver.lua
+++ lua/configs/lsp/settings/tsserver.lua
@@ -1,9 +1,10 @@
 local M = {}
 
 local lsputils = require("configs.lsp.utils")
+local typescript = require("typescript")
 
 function M.setup()
-	require("typescript").setup({
+	typescript.setup({
 		disable_commands = false,
 		debug = false,
 		server = {
