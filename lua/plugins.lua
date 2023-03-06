43e6f2a4e0c96930857064c9c6700bb064bc3ad1
Edward <edward_soetiono@estl.sg>
2023-03-06 09:59:49 +0800

    switched to ts-rainbow2

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -128,7 +128,7 @@ packer.startup(function()
 	-- Treesitter
 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 	use("nvim-treesitter/nvim-treesitter-textobjects")
-	use("p00f/nvim-ts-rainbow")
+	use("HiPhish/nvim-ts-rainbow2")
 	use("windwp/nvim-ts-autotag")
 	use("JoosepAlviste/nvim-ts-context-commentstring")
 
