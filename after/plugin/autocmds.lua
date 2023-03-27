548e226858c29b8d699be01563269a38ce5cee24
Edward <edward_soetiono@estl.sg>
2023-03-27 14:29:53 +0800

    neotest fast quit

--- after/plugin/autocmds.lua
+++ after/plugin/autocmds.lua
@@ -14,3 +14,16 @@ api.nvim_create_autocmd({ "BufWinEnter" }, {
 		vim.cmd("set formatoptions-=cro")
 	end,
 })
+
+local neotestConfigGroup = api.nvim_create_augroup("NeotestConfig", {})
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "neotest-output",
+	group = neotestConfigGroup,
+	callback = function(opts)
+		vim.keymap.set("n", "q", function()
+			pcall(vim.api.nvim_win_close, 0, true)
+		end, {
+			buffer = opts.buf,
+		})
+	end,
+})
