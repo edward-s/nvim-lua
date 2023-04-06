dbeeb065dfc51fc8b214b9b0d7be552be4334ab3
Edward <edward_soetiono@estl.sg>
2023-04-07 04:45:53 +0800

    added dial

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,6 +50,19 @@ return {
 		"famiu/bufdelete.nvim",
 	},
 	{
+		"monaqa/dial.nvim",
+		keys = { "<C-a>", "<C-x>", { "<C-a>", "v" }, { "<C-x>", "v" }, { "g<C-a>", "v" }, { "g<C-x>", "v" } },
+     -- stylua: ignore
+     init = function()
+       vim.api.nvim_set_keymap("n", "<C-a>", require("dial.map").inc_normal(), { desc = "Increment", noremap = true })
+       vim.api.nvim_set_keymap("n", "<C-x>", require("dial.map").dec_normal(), { desc = "Decrement", noremap = true })
+       vim.api.nvim_set_keymap("v", "<C-a>", require("dial.map").inc_visual(), { desc = "Increment", noremap = true })
+       vim.api.nvim_set_keymap("v", "<C-x>", require("dial.map").dec_visual(), { desc = "Decrement", noremap = true })
+       vim.api.nvim_set_keymap("v", "g<C-a>", require("dial.map").inc_gvisual(), { desc = "Increment", noremap = true })
+       vim.api.nvim_set_keymap("v", "g<C-x>", require("dial.map").dec_gvisual(), { desc = "Decrement", noremap = true })
+     end,
+	},
+	{
 		"RRethy/vim-illuminate",
 		event = { "BufReadPost", "BufNewFile" },
 		opts = { delay = 200 },
