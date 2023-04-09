ef29e47eca01a1b7b9ede4cfbf16056edcb35279
Edward <edward_soetiono@estl.sg>
2023-04-09 18:35:21 +0800

    gruvbox

--- lua/config/colorschemes/gruvbox.lua
+++ lua/config/colorschemes/gruvbox.lua
@@ -1,18 +1,20 @@
+vim.g.gruvbox_material_palette = "material"
+vim.g.gruvbox_material_background = "medium"
+vim.g.gruvbox_material_visual = "green background"
+
 vim.cmd([[ 
 function! s:gruvbox_material_custom() abort
-  highlight! link DiagnosticWarn None
-  highlight! link DiagnosticError None
-  highlight! link DiagnosticInfo None
-  highlight! link DiagnosticHint None
-  highlight! link DefinitionSearch None
+  hi! link DiagnosticWarn None
+  hi! link DiagnosticError None
+  hi! link DiagnosticInfo None
+  hi! link DiagnosticHint None
+  hi! link DefinitionSearch None
 endfunction
 
 augroup GruvboxMaterialCustom
   autocmd!
   autocmd ColorScheme gruvbox-material call s:gruvbox_material_custom()
 augroup END
-
-let g:gruvbox_material_visual = 'green background'
 
 colorscheme gruvbox-material
 ]])
