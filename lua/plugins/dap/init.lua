4fe50a6f9a00ac93cfecc7641b0205833f41eb2f
Edward <sedward@live.com.sg>
2023-04-13 13:50:24 +0800

    fixed dap

--- lua/plugins/dap/init.lua
+++ lua/plugins/dap/init.lua
@@ -15,8 +15,8 @@ return {
     { "<leader>dd", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
     { "<leader>dh", function() require("dap.ui.widgets").hover() end, desc = "Hover Variables" },
     { "<leader>du", function() require("dapui").toggle() end, desc = "Dap UI" },
-    { "<leader>di", function() require("dap.ui.widgets").step_into() end, desc = "Step Into" },
-    { "<leader>do", function() require("dap.ui.widgets").step_out() end, desc = "Step Out" },
+    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+    { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
     { "<leader>ds", function() require("dap").step_over() end, desc = "Step Over" },
     { "<leader>dq", function() require("dap").terminate() end, desc = "Terminate" },
   },
@@ -38,7 +38,6 @@ return {
 			dapui.setup()
 
 			dap.listeners.after.event_initialized["dapui_config"] = function()
-				vim.cmd(":Neotree close")
 				dapui.open()
 			end
 			dap.listeners.before.event_terminated["dapui_config"] = function()
