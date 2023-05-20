f56ca33e915126c52606931f3b144bde0276c4cf
Edward <sedward@live.com.sg>
2023-05-20 12:17:44 +0800

    upped libs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -14,7 +14,16 @@ return {
 				show_buffer_close_icons = false,
 				show_close_icon = false,
 				color_icons = true,
-				offsets = { { filetype = "NvimTree", text = "" } },
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = function()
+							return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+						end,
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
 			},
 		},
 	},
