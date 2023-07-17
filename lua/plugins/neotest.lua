fcf15998859419d7835f6d7fdca907480f585052
Edward <sedward@live.com.sg>
2023-07-17 23:09:40 +0800

    added neotest output panel

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -7,7 +7,12 @@ return {
 		{ "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "File" },
 		{ "<leader>tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Run Last" },
 		{ "<leader>tm", "<cmd>lua require('neotest').summary.run_marked()<cr>", desc = "Run Marked" },
-		{ "<leader>to", "<cmd>lua require('neotest').output.open({ enter = true })<cr>", desc = "Output" },
+		{
+			"<leader>to",
+			"<cmd>lua require('neotest').output.open({ enter = true, auto_close = true })<cr>",
+			desc = "Output",
+		},
+		{ "<leader>tO", "<cmd>lua require('neotest').output_panel.toggle()<cr>", desc = "Toggle Output Panel" },
 		{ "<leader>tq", "<cmd>lua require('neotest').run.stop()<cr>", desc = "Stop" },
 		{ "<leader>ts", "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Summary" },
 		{ "<leader>tt", "<cmd>lua require('neotest').run.run()<cr>", desc = "Nearest" },
@@ -29,7 +34,7 @@ return {
 				enabled = false,
 			},
 			output = {
-				open_on_run = false,
+				open_on_run = true,
 			},
 			quickfix = {
 				enabled = false,
