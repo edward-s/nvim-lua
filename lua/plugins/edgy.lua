3306cccac463404d423407908f34c9c47ac7f3de
Edward <sedward@live.com.sg>
2023-07-19 10:44:36 +0800

    re-added edgy

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -2,8 +2,26 @@ return {
 	"folke/edgy.nvim",
 	event = "VeryLazy",
 	opts = {
+		wo = {
+			winhighlight = "",
+		},
 		bottom = {
+			"Trouble",
 			{ ft = "qf", title = "Quickfix" },
+			{ ft = "spectre_panel", size = { height = 0.4 } },
+			{
+				ft = "help",
+				size = { height = 20 },
+				-- don't open help files in edgy that we're editing
+				filter = function(buf)
+					return vim.bo[buf].buftype == "help"
+				end,
+			},
+			{ title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
+		},
+		right = {
+			{ ft = "neotest-summary", title = "Neotest Summary", size = { width = 0.15 } },
+			{ ft = "aerial", title = "Code Outline" },
 		},
 	},
 }
