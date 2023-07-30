ad3257ad113a09be401ba62087d45d0ca7a3f6b5
Edward <sedward@live.com.sg>
2023-07-30 09:50:20 +0800

    cleaned up dap

--- lua/plugins/dap/init.lua
+++ lua/plugins/dap/init.lua
@@ -2,19 +2,46 @@ return {
 	{
 		"mfussenegger/nvim-dap",
 		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
+			{
+				"rcarriga/nvim-dap-ui",
+        -- stylua: ignore
+        keys = {
+          { "<leader>e", function() require("dapui").eval() end, desc = "Dap Eval" },
+          { "<leader>du", function() require("dapui").toggle() end, desc = "Dap UI" },
+        },
+				config = function()
+					local dap = require("dap")
+					local dapui = require("dapui")
+					dapui.setup()
+
+					dap.listeners.after.event_initialized["dapui_config"] = function()
+						vim.cmd("NvimTreeClose")
+						vim.defer_fn(function()
+							dapui.open()
+						end, 200)
+					end
+					dap.listeners.before.event_terminated["dapui_config"] = function()
+						dapui.close()
+					end
+					dap.listeners.before.event_exited["dapui_config"] = function()
+						dapui.close()
+					end
+				end,
+			},
+			{
+				"theHamsta/nvim-dap-virtual-text",
+				opts = {
+					commented = true,
+				},
+			},
 			"mxsdev/nvim-dap-vscode-js",
 		},
   -- stylua: ignore
   keys = {
-    { "<leader>e", function() require("dapui").eval() end, desc = "Dap Eval" },
     { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
     { "<leader>dx", function() require("dap").clear_breakpoints() end, desc = "Clear breakpoints" },
     { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
     { "<leader>dd", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
-    { "<leader>dh", function() require("dap.ui.widgets").hover() end, desc = "Hover Variables" },
-    { "<leader>du", function() require("dapui").toggle() end, desc = "Dap UI" },
     { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
     { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
     { "<leader>ds", function() require("dap").step_over() end, desc = "Step Over" },
@@ -29,26 +56,6 @@ return {
 		},
 		config = function(plugin, opts)
 			vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "error", linehl = "", numhl = "" })
-
-			require("nvim-dap-virtual-text").setup({
-				commented = true,
-			})
-
-			local dap, dapui = require("dap"), require("dapui")
-			dapui.setup()
-
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				vim.cmd("NvimTreeClose")
-				vim.defer_fn(function()
-					dapui.open()
-				end, 200)
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
 
 			for k, _ in pairs(opts.setup) do
 				opts.setup[k](plugin, opts)
