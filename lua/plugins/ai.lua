5924e28c1abcbb90c12db59aa869b2898317c55c
Edward <sedward@live.com.sg>
2023-08-09 19:25:50 +0800

    CGPT tweaks

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -5,12 +5,25 @@ return {
     -- stylua: ignore
     keys = {
       { "<leader>ac", "<cmd>ChatGPT<cr>", desc = "ChatGPT" },
+      { "<leader>ae", function() require('chatgpt').edit_with_instructions() end, mode = "v", desc = "ChatGPT With Instructions" },
     },
 		opts = {
+			edit_with_instructions = {
+				diff = true,
+			},
 			chat = {
 				keymaps = {
 					close = "q",
 				},
+			},
+			openai_params = {
+				model = "gpt-3.5-turbo",
+				frequency_penalty = 0,
+				presence_penalty = 0,
+				max_tokens = 3000,
+				temperature = 0,
+				top_p = 1,
+				n = 1,
 			},
 		},
 		config = function(_, opts)
