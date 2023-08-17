b7b00dbfd1aa9eb1058e70bb923058b493e8f9fc
Edward <sedward@live.com.sg>
2023-08-17 17:09:52 +0800

    fixed illuminate configs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -86,11 +86,12 @@ return {
 		"RRethy/vim-illuminate",
 		event = { "BufReadPost", "BufNewFile" },
 		opts = {
+			providers = {
+				"lsp",
+				"treesitter",
+			},
 			delay = 200,
 			large_file_cutoff = 2000,
-			large_file_overrides = {
-				providers = { "lsp" },
-			},
 		},
 		config = function(_, opts)
 			require("illuminate").configure(opts)
