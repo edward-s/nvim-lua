4fdc0f04a04d98a6b61ee6b41d342075f2dda220
Edward <sedward@live.com.sg>
2023-08-22 19:57:58 +0800

    cgpt configs

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -25,6 +25,15 @@ return {
 				submit = "<Enter>",
 				submit_n = "<Enter>",
 			},
+			openai_params = {
+				model = "gpt-4",
+				frequency_penalty = 0,
+				presence_penalty = 0,
+				max_tokens = 3000,
+				temperature = 0,
+				top_p = 1,
+				n = 1,
+			},
 		},
 		config = function(_, opts)
 			require("chatgpt").setup(opts)
