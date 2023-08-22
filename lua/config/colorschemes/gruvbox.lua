d2a0ff0dcd89c0c4f9ef31e4b7b2ff997f9a479d
Edward <sedward@live.com.sg>
2023-08-22 21:32:13 +0800

    removed all custom diagnostics colors

--- lua/config/colorschemes/gruvbox.lua
+++ lua/config/colorschemes/gruvbox.lua
@@ -5,11 +5,6 @@ vim.g.gruvbox_material_visual = "green background"
 vim.cmd([[ 
 function! s:gruvbox_material_custom() abort
   hi! link FloatBorder Normal
-  hi! link DiagnosticWarn None
-  hi! link DiagnosticError None
-  hi! link DiagnosticInfo None
-  hi! link DiagnosticHint None
-  hi! link DefinitionSearch None
 endfunction
 
 augroup GruvboxMaterialCustom
