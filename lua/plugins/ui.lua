eaf24d705dd92f0853e1ba4e7352b121ab26532a
Edward <sedward@live.com.sg>
2023-08-24 21:28:46 +0800

    added rainbow parantheses

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -38,4 +38,15 @@ return {
 		},
 		event = { "VeryLazy" },
 	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = { "HiPhish/nvim-ts-rainbow2" },
+		opts = function(_, opts)
+			opts.rainbow = {
+				enable = true,
+				query = "rainbow-parens",
+				strategy = require("ts-rainbow").strategy.global,
+			}
+		end,
+	},
 }
