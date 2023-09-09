dc52ca91a4e84fc46fefaac9dafc7cf27e368f30
Edward <sedward@live.com.sg>
2023-09-09 21:48:55 +0800

    re-enabled bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,17 +1,30 @@
 return {
 	{
 		"akinsho/nvim-bufferline.lua",
-		lazy = false,
+		event = "VeryLazy",
 		keys = {
 			{ "<leader>bp", "<cmd>BufferLinePick<cr>", desc = "Pick Buffer" },
 		},
 		opts = {
 			options = {
-				mode = "tabs",
+				mode = "buffers",
+				always_show_bufferline = false,
+				diagnostics = "nvim_lsp",
+				separator_style = "thin",
 				show_tab_indicators = true,
 				show_buffer_close_icons = false,
 				show_close_icon = false,
-				always_show_bufferline = false,
+				color_icons = true,
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = function()
+							return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+						end,
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
 			},
 		},
 	},
