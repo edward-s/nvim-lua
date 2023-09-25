d01ccea10a76c6da429afd8b9098864d24706791
Edward <sedward@live.com.sg>
2023-09-25 20:28:06 +0800

    neotest changes

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -9,7 +9,7 @@ return {
 		{ "<leader>tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Run Last" },
 		{ "<leader>tm", "<cmd>lua require('neotest').summary.run_marked()<cr>", desc = "Run Marked" },
 		{ "<leader>to", "<cmd>lua require('neotest').output.open({ enter = true, auto_close = true })<cr>", desc = "Output" },
-		{ "<leader>tO", "<cmd>lua require('neotest').output_panel.toggle()<cr>", desc = "Toggle Output Panel" },
+		{ "<leader>tc", "<cmd>lua require('neotest').output_panel.toggle()<cr>", desc = "Toggle Output Panel" },
 		{ "<leader>tq", "<cmd>lua require('neotest').run.stop()<cr>", desc = "Stop" },
 		{ "<leader>ts", "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Summary" },
 		{ "<leader>tt", "<cmd>lua require('neotest').run.run()<cr>", desc = "Nearest" },
@@ -18,27 +18,32 @@ return {
 		"haydenmeade/neotest-jest",
 		"mfussenegger/nvim-dap",
 	},
-	config = function()
-		local opts = {
-			icons = {
-				running = "↻",
-			},
+	opts = function()
+		return {
 			adapters = {
 				require("neotest-jest"),
 			},
 			diagnostic = {
 				enabled = false,
 			},
-			output = {
-				open_on_run = false,
-			},
-			status = {
-				virtual_text = true,
-			},
 			floating = {
 				options = {
 					wrap = true,
 				},
+			},
+			icons = {
+				running = "↻",
+			},
+			output = {
+				open_on_run = false,
+			},
+			quickfix = {
+				open = function()
+					vim.cmd("Trouble quickfix")
+				end,
+			},
+			status = {
+				virtual_text = true,
 			},
 			summary = {
 				mappings = {
@@ -47,6 +52,14 @@ return {
 				},
 			},
 		}
+	end,
+	config = function(_, opts)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "neotest-output-panel",
+			callback = function()
+				vim.cmd("norm G")
+			end,
+		})
 		require("neotest").setup(opts)
 	end,
 }
