52fd9617c0f5b33c8f6eb2fa6653a0a8df7df2ed
Edward <sedward@live.com.sg>
2023-12-02 18:44:47 +0800

    Revert "fixed toggleterm command"
    This reverts commit b7bff91168a7021fe7a20b9539f614740acb729b.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -17,6 +17,12 @@ return {
 			start_in_insert = true,
 			persist_mode = true,
 			direction = "float",
+			on_open = function(term)
+				if term.cmd ~= "lazygit" then
+					local opts = { noremap = true }
+					vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+				end
+			end,
 			float_opts = {
 				border = "curved",
 				winblend = 0,
