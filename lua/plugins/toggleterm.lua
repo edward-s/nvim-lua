67adf5b946ff529d813adf532f74882bb621a67b
Edward <sedward@live.com.sg>
2023-12-03 12:56:33 +0800

    toggleterm fixes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -13,22 +13,13 @@ return {
 			autochdir = true,
 			size = 20,
 			open_mapping = [[<C-t>]],
-			close_on_exit = true,
-			start_in_insert = true,
-			persist_mode = true,
 			direction = "float",
 			on_open = function(term)
 				if term.cmd ~= "lazygit" then
 					local opts = { noremap = true }
-					vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+					vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
 				end
 			end,
-			float_opts = {
-				border = "curved",
-				winblend = 0,
-				width = math.floor(0.95 * vim.fn.winwidth(0)),
-				height = math.floor(0.9 * vim.fn.winheight(0)),
-			},
 		},
 	},
 }
